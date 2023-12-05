e94106010807732b6e197b60f779792d5b1817ec
SamEThibault <sam5thibault@gmail.com>
2023-12-05 14:51:45 -0500

    part 2 added

--- D1/main.py
+++ D1/main.py
@@ -4,11 +4,35 @@ entries = file.readlines()
 total = 0
 digits = []
 
+d = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+
 for e in entries:
+    alpha_digit = ""
     for char in e:
         if char.isnumeric():
             digits.append(char)
+        else:
+            alpha_digit += char
+        
+        for key in list(d.keys()):
+            if key in alpha_digit:
+                digits.append(d[key])
+                alpha_digit = ""
+                break
+
     total += int(digits[0] + digits[-1])
     digits.clear()
+
 
 print(total)
